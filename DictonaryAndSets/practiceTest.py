d6d2ef258d8e718d588188b4d7ea59fbe71288d5
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-11-15 18:56:24 +0530

    Conditioanl statemetn

--- DictonaryAndSets/practiceTest.py
+++ DictonaryAndSets/practiceTest.py
@@ -1,10 +1,17 @@
 # 1. Write a program to create a dictonary with hindi and english word in it
 
-words = {
-    "apple" : "seb",
-    "banana" : "kela",
-    "cherry" : "amchur"
-}
+# words = {
+#     "apple" : "seb",
+#     "banana" : "kela",
+#     "cherry" : "amchur"
+# }
 
-word = input("enter the word ")
-print (words[word])
+# word = input("enter the word ")
+# print (words[word])
+
+# # 2. add no in set
+# s = set()
+# n = input("enter no: ")
+# s. add(int (n))
+
+# 3.  
---
+++ conditionalExpression/conditionalIntro.py
@@ -0,0 +1,20 @@
+a = 2
+b = 56
+
+
+if(a<b):
+    print("this is seperate if statement")
+
+
+    #This is seperate if statement
+if(a>12):
+    print("a is greater than 12")
+elif(a<b):
+    print("b is breater")
+
+else:
+    print("b is greater than 12")
+#till here 
+
+
+
